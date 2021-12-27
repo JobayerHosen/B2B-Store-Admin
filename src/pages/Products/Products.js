b28d0961da8164d25dd8584de873778c2e0ef229
Jobayer Hosen <juba2732@gmail.com>
2021-12-27 23:12:51 +0600

    feat(api): add products api data to products component via productSlice reducer

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { addProducts, getAllProducts } from "../../app/slices/productSlice";
 import { Navigation } from "../../components";
+import productsApi from "../../services/productsApi";
 
 const Products = () => {
+  const products = useSelector(getAllProducts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await productsApi.get("/products.json").catch((err) => {
+        console.log(err);
+      });
+      dispatch(addProducts(response?.data));
+    };
+
+    fetchProducts();
+  }, [dispatch]);
+
   return (
     <div className="view">
       {/*-------------PAGE HEADER-------------*/}
@@ -27,6 +45,7 @@ const Products = () => {
         {/* ------------MAIN CONTENT------------*/}
         <div className="content">
           <h1>Products</h1>
+          {products && products.map((product) => <li>{product?.title}</li>)}
         </div>
         {/* ------------------------------------ */}
       </main>
