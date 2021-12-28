407cfc235d2af5ba1162992100b23d99abefd826
Jobayer Hosen <juba2732@gmail.com>
2021-12-29 00:12:53 +0600

    feat(component): add overview section in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import { Navigation } from "../../components";
 
 const Home = () => {
@@ -22,7 +23,41 @@ const Home = () => {
 
         {/* ------------MAIN CONTENT------------*/}
         <div className="content">
-          <h3>Home</h3>
+          {/* OVERVIEW SECTION  */}
+          <section className="overview mb-3">
+            <div className="d-flex justify-content-between">
+              <h5>Overview</h5>
+              <p className="text-muted">
+                Details <i className="bi bi-chevron-right"></i>
+              </p>
+            </div>
+            <Row>
+              <Col xs="6" md="3">
+                <div className="rounded shadow p-3 mb-3">
+                  <h6 className="text-muted text-uppercase">Active Orders</h6>
+                  <h3>35</h3>
+                </div>
+              </Col>
+              <Col xs="6" md="3">
+                <div className="rounded shadow p-3 mb-3">
+                  <h6 className="text-muted text-uppercase">Total Orders</h6>
+                  <h3>340</h3>
+                </div>
+              </Col>
+              <Col xs="6" md="3">
+                <div className="rounded shadow p-3 mb-3">
+                  <h6 className="text-muted text-uppercase">Total Sales</h6>
+                  <h3>$2752</h3>
+                </div>
+              </Col>
+              <Col xs="6" md="3">
+                <div className="rounded shadow p-3 mb-3">
+                  <h6 className="text-muted text-uppercase">Product View</h6>
+                  <h3>4350</h3>
+                </div>
+              </Col>
+            </Row>
+          </section>
         </div>
         {/* ------------------------------------ */}
       </main>
