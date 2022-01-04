f7abae32e369bf6a10cc5885e609af8953029d01
Jobayer Hosen <juba2732@gmail.com>
2022-01-04 21:39:11 +0600

    feat(component): add avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Avatar.css";
+
+const Avatar = () => {
+  return (
+    <div className="avatar">
+      <img src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg" alt="" />
+    </div>
+  );
+};
+
+export default Avatar;
